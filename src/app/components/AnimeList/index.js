86b499bd02cf3978f008794180ec95ec3b53c2e6
arenga <arengapinnatamarzuki@gmail.com>
2024-08-03 20:22:57 +0700

    change css style

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const AnimeList = ({ title, images, id }) => {
   return (
-    <Link href={`/${id}`} className="shadow-xl">
+    <Link href={`/${id}`} className="card shadow-xl">
       <Image src={images} width={300} height={300} alt="..." />
       <h2 className="font-bold md:text-xl text-md p-4">{title}</h2>
     </Link>
